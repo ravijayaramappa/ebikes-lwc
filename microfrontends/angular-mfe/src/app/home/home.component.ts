8b57629a5f4fab520689a223f5de15f2a4ed0e58
Manuel Jasso <manuel.jasso@salesforce.com>
2025-10-27 12:02:25 -0700

    Integrated c/similarProducts into LO2

--- microfrontends/angular-mfe/src/app/home/home.component.ts
+++ microfrontends/angular-mfe/src/app/home/home.component.ts
@@ -4,11 +4,11 @@ import { RegisterComponent } from '../register/register.component';
 import { DealerLocatorComponent } from '../dealer-locator/dealer-locator.component';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule, RegisterComponent, DealerLocatorComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  template: `
+    selector: 'app-home',
+    standalone: true,
+    imports: [CommonModule, RegisterComponent, DealerLocatorComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    template: `
     <div class="card">
       <h2>Welcome to the Angular Microfrontend</h2>
       <p>
@@ -17,8 +17,8 @@ import { DealerLocatorComponent } from '../dealer-locator/dealer-locator.compone
         LWC will be mounted.
       </p>
       <div id="lo2-root" style="min-height: 240px; border: 2px dashed #d1d5db; border-radius: 8px; display:flex; align-items:center; justify-content:center;">
-        <lightning-out-application components="c-product-card" id="loApp"></lightning-out-application>
-        <c-product-card id="loComp"></c-product-card>
+        <lightning-out-application components="c-similar-products" id="loApp"></lightning-out-application>
+        <c-similar-products id="loComp" style="height:302px"></c-similar-products>
       </div>
     </div>
 
@@ -36,10 +36,9 @@ import { DealerLocatorComponent } from '../dealer-locator/dealer-locator.compone
 export class HomeComponent {
     ngOnInit() {
         const loApp = document.getElementById("loApp") as any;
+        loApp.orgUrl = "https://dvagworkshopdemoorg-dev-ed.sfdctest.test1.lightning.pc-rnd.force.com";
+
         const loComp = document.getElementById("loComp") as any;
-        if (loApp && loComp) {
-            loApp.orgUrl = "https://dvagworkshopdemoorg-dev-ed.sfdctest.test1.lightning.pc-rnd.force.com";
-            loComp.recordId = "a03SM00000AR4MuYAL";
-        }
+        loComp.recordId = "a03SM00000AR4MuYAL";
     }
 }
