485537bc99a1ca8f12586e10e34008faca7fd09d
Manuel Jasso <manuel.jasso@salesforce.com>
2025-10-26 21:42:53 -0700

    Simple initial LO2 integration
    # Conflicts:
    #	microfrontends/angular-mfe/src/app/home/home.component.ts

--- microfrontends/angular-mfe/src/app/home/home.component.ts
+++ microfrontends/angular-mfe/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from '../register/register.component';
 import { DealerLocatorComponent } from '../dealer-locator/dealer-locator.component';
@@ -7,6 +7,7 @@ import { DealerLocatorComponent } from '../dealer-locator/dealer-locator.compone
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, RegisterComponent, DealerLocatorComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   template: `
     <div class="card">
       <h2>Welcome to the Angular Microfrontend</h2>
@@ -16,7 +17,8 @@ import { DealerLocatorComponent } from '../dealer-locator/dealer-locator.compone
         LWC will be mounted.
       </p>
       <div id="lo2-root" style="min-height: 240px; border: 2px dashed #d1d5db; border-radius: 8px; display:flex; align-items:center; justify-content:center;">
-        <span>Lightning Out 2.0 placeholder</span>
+        <lightning-out-application components="c-product-card" id="loApp"></lightning-out-application>
+        <c-product-card id="loComp"></c-product-card>
       </div>
     </div>
 
@@ -31,6 +33,13 @@ import { DealerLocatorComponent } from '../dealer-locator/dealer-locator.compone
     </div>
   `
 })
-export class HomeComponent {}
-
-
+export class HomeComponent {
+    ngOnInit() {
+        const loApp = document.getElementById("loApp") as any;
+        const loComp = document.getElementById("loComp") as any;
+        if (loApp && loComp) {
+            loApp.orgUrl = "https://dvagworkshopdemoorg-dev-ed.sfdctest.test1.lightning.pc-rnd.force.com";
+            loComp.recordId = "a03SM00000AR4MuYAL";
+        }
+    }
+}
